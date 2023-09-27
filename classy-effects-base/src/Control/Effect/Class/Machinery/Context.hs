cef92b1f1d5a0f56adee0e7d8be2a5211bf85246
Yamada Ryo <ymdfield@outlook.jp>
2023-09-27 15:14:58 +0900

    [add] documentation about the extensible functorial context.

--- classy-effects-base/src/Control/Effect/Class/Machinery/Context.hs
+++ classy-effects-base/src/Control/Effect/Class/Machinery/Context.hs
@@ -13,7 +13,7 @@ Portability :  portable
 
 Enable localized effect handling.
 
-For example, for an interface like @a -> f (Either e b)@, expand @Either e a@ as a @Throw@ effect to
+For example, for an interface like @a -> f (Either e b)@, expand @Either e@ as a @Throw@ effect to
 @f@, automatically generating @Throw e f => a -> f b@ from @a -> f (Either e b)@.
 -}
 module Control.Effect.Class.Machinery.Context where
@@ -33,6 +33,24 @@ class FunctorialContext cTag ctx (f :: Type -> Type) where
 {- |
 Reflect the /functorial context/ @ctx@ into the carrier @f@, using the Boehm-Berarducci encoding for
 extensibility.
+
+In general, for an effect class @E@, a functorial context @ctx@, and an @applyContext@ that
+translates @ctx@ into any carrier under the @E@ constraint, when every @E@-based effect
+@f :: A -> f B@ issued in @applyContext@ has an interface @A -> f B@ that is /divisible/ with
+respect to the effect class parameter @o@, such a functorial context is considered extensible.
+
+Here, by /divisible/, we mean that when @o@ is composed as a direct sum @o=o_1+o_2+...+o_n@, a
+variant of @f@, denoted @f_i@, where any occurrence of @o@ in the interface of @f@ has been replaced
+by any @o_i@, can be derived from @f@ (a function exists that maps @f@ to @f_i@). This is
+reminiscent of projection operations, and divisibility likely equates to the interface being of the
+form @o -> ...@.
+In this case, by combining the @...@ parts for all effects @f@ issued in @applyContext@ as a direct
+product, it is expected to constitute the extensible final type @r@.
+For instance, the forms of effects like @throw@ or @put@ are straightforward to grasp. However, a
+slightly more complex example would be the @These@ monad (@Chronicle@ effect class). Since
+@applyContext@ can issue two effects, @dictate@ and @confess@, @r@ would be a pair of their @...@
+parts, resulting in @(m a, m ())@, where @m@ is the carrier. For more details, refer to
+@Control.Effect.Class.Chronicle@ in the @classy-effects@ package.
 -}
 class
     FunctorialContext cTag (ctx o) f =>
